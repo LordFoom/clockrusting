58a0c8dfc889cdd95a37f2048b0344895e702760
lordfoom <timothys@soliditech.com>
2022-01-15 13:21:46 +0200

    Initial commit, want to clockrust as lib

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Decided time tracking is something I want includable.
+
+UDP server, I guess was an okay idea to begin with, but actually, a bit crazy.
---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+///Library for adding time tracking to an application
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
