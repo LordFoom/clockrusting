062aa38e548c189f23c00a16ea2af951960966dc
lordfoom <timothys@soliditech.com>
2022-02-07 18:57:39 +0200

    Take out a space

--- src/output.rs
+++ src/output.rs
@@ -2,7 +2,7 @@ use std::fs;
 use color_eyre::Report;
 use crate::command::Command;
 
-///Write out a neat  little file with our time tracking report
+///Write out a neat little file with our time tracking report
 pub fn write_tracking_report(file_path: &str, cmd_list:&Vec<Command>) -> Result<(), Report> {
     let mut file_contents = String::new();
     cmd_list.iter()
