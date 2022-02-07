7678c4d1b529afe2008172b52723601b7a9de2b3
lordfoom <timothys@soliditech.com>
2022-02-07 18:57:23 +0200

    Fix it up a little so it, you know, compiles

--- src/output.rs
+++ src/output.rs
@@ -1,10 +1,9 @@
-use std::fs::File;
+use std::fs;
 use color_eyre::Report;
 use crate::command::Command;
 
 ///Write out a neat  little file with our time tracking report
 pub fn write_tracking_report(file_path: &str, cmd_list:&Vec<Command>) -> Result<(), Report> {
-    let mut output = File::create(file_path);
     let mut file_contents = String::new();
     cmd_list.iter()
             .for_each(|cmd| {
@@ -12,5 +11,6 @@ pub fn write_tracking_report(file_path: &str, cmd_list:&Vec<Command>) -> Result<
                 file_contents.push('\n');
             });
 
-    write!(output, file_contents)?
+    fs::write(file_path, file_contents)?;
+    Ok(())
 }
